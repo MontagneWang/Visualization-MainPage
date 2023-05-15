2bc1fa04fc14af641348e0a80f84aeef98d2efac
山遥路远 <2214981880@qq.com>
2023-05-15 12:46:57 +0800

    前后端接口 初步构建 post 连接

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 	baseURL: '/api', // 更改为您的 API 基本路径
+	timeout: 5000
 	// 其他可选配置
 });
 
--- src/views/DataVisualization.vue
+++ src/views/DataVisualization.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import axios from '../api/axios';
-import {onMounted} from "vue";
+import {onMounted, ref, toRaw, watchEffect} from "vue";
 // import {Songs} from "../api/getSongData"; // 根据您的项目路径进行调整
 // import { ref, onMounted } from 'vue';
 // import { getSongs, Songs } from '../api/getSongData';
@@ -13,20 +13,61 @@ import {onMounted} from "vue";
 // 	// console.log(songs.value.data.desc)
 // });
 
-onMounted(async () => {
+let time = ref(new Date())
+setInterval(() => {
+	time.value = new Date();
+}, 1000);
+
+const updateData = async (id=314772,title=time.value.toLocaleString()) => {
 	try {
-		// const {data: {data:res}} = await axios.get('/x/web-interface/view?bvid=BV1BM411k7MS');
-		const {data:res} = await axios.get('/Midterm/getdata.php?page=18');
-		console.log(res)
+		const response = await axios.post('/Visualization/tempTest/getdata.php', {
+			id: id,
+			title: title
+		});
+		console.log(response.data);
+		await getData()
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+let res = ref([
+	{id:0,title:'',play:0}
+])
+
+async function getData() {
+	try {
+		const {data: response} = await axios.get('/Visualization/tempTest/getdata.php?page=1');
+		res.value = response
+		console.log(toRaw(res.value))
 	} catch (error) {
 		console.error('Error fetching songs:', error);
 	}
+}
+watchEffect(()=>{
+	updateData()
+})
+onMounted(()=>{
+	getData()
 });
+
 </script>
 
 <template>
 	<h1>这里是 DataVisualization</h1>
-	</template>
+	<ul>
+		<li v-for="song in res">
+			{{ song.title }} - {{ song.play }}
+		</li>
+	</ul>
+	<hr>
+	<ul>
+		<li v-for="song in res">
+			<input style="width: 76%" v-model="song.title" />
+			<input style="width: 20%" v-model="song.play" />
+		</li>
+	</ul>
+</template>
 
 <style scoped>
 body {
