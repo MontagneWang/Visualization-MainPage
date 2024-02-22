7b55878ab930b4021c5124fbf4ced8f93258218b
山遥路远 <2214981880@qq.com>
2024-02-22 21:47:10 +0800

    feat: 移动端横屏适配

--- src/App.vue
+++ src/App.vue
@@ -4,109 +4,9 @@ import TheBackTop from "./components/TheBackTop.vue";
 import TheCircularNav from "./components/TheCircularNav.vue";
 import TheRightMenu from "./components/TheRightMenu.vue";
 import IntroLing from "./views/IntroLing.vue";
-// 跳转到 ling 路由
+import { onMounted } from "vue";
 const router = useRouter();
 // router.push('/message')
-
-// 标题切换
-// let timer: number | undefined
-// let title: string | undefined
-// document.addEventListener('visibilitychange', function () {
-// 	if (document.visibilityState === 'hidden') {
-// 		if (document.title !== '欢迎回来') {
-// 			title = document.title
-// 		}
-// 		document.title = '我在这里等你'
-// 		clearTimeout(timer)
-// 	}
-// 	// 可以直接 else if
-// 	if (document.visibilityState === 'visible') {
-// 		document.title = '欢迎回来'
-// 		if (title === undefined) {
-// 			title = 'About Ling'
-// 		}
-// 		timer = setTimeout('document.title = title', 1000)
-// 		timer = setTimeout('document.title = \'About Ling\'', 1000)
-// 	}
-// })
-
-function getPerformanceTiming() {
-  let performance = window.performance;
-
-  if (!performance) {
-    // 当前浏览器不支持
-    console.log("浏览器不支持 performance 接口");
-    return;
-  }
-
-  let t = performance.timing;
-  let times = {
-    loadPage: "",
-    domReady: "",
-    redirect: "",
-    lookupDomain: "",
-    ttfb: "",
-    request: "",
-    loadEvent: "",
-    appcache: "",
-    unloadEvent: "",
-    connect: "",
-  };
-
-  //【重要】页面加载完成的时间
-  //【原因】这几乎代表了用户等待页面可用的时间
-  times.loadPage = `页面加载完成的时间：${t.loadEventEnd - t.navigationStart}`;
-
-  //【重要】解析 DOM 树结构的时间
-  //【原因】反省下你的 DOM 树嵌套是不是太多了！
-  times.domReady = `解析 DOM 树结构的时间：${t.domComplete - t.responseEnd}`;
-
-  //【重要】重定向的时间
-  //【原因】拒绝重定向！比如，http://example.com/ 就不该写成 http://example.com
-  times.redirect = `重定向的时间：${t.redirectEnd - t.redirectStart}`;
-
-  //【重要】DNS 查询时间
-  //【原因】DNS 预加载做了么？页面内是不是使用了太多不同的域名导致域名查询的时间太长？
-  // 可使用 HTML5 Prefetch 预查询 DNS ，见：[HTML5 prefetch](http://segmentfault.com/a/1190000000633364)
-  times.lookupDomain = `DNS 查询时间：${
-    t.domainLookupEnd - t.domainLookupStart
-  }`;
-
-  //【重要】读取页面第一个字节的时间
-  //【原因】这可以理解为用户拿到你的资源占用的时间，加异地机房了么，加CDN 处理了么？加带宽了么？加 CPU 运算速度了么？
-  // TTFB 即 Time To First Byte 的意思
-  // 维基百科：https://en.wikipedia.org/wiki/Time_To_First_Byte
-  times.ttfb = `读取页面第一个字节的时间：${
-    t.responseStart - t.navigationStart
-  }`;
-
-  //【重要】内容加载完成的时间
-  //【原因】页面内容经过 gzip 压缩了么，静态资源 css/js 等压缩了么？
-  times.request = `内容加载完成的时间：${t.responseEnd - t.requestStart}`;
-
-  //【重要】执行 onload 回调函数的时间
-  //【原因】是否太多不必要的操作都放到 onload 回调函数里执行了，考虑过延迟加载、按需加载的策略么？
-  times.loadEvent = `执行 onload 回调函数的时间：${
-    t.loadEventEnd - t.loadEventStart
-  }`;
-
-  // DNS 缓存时间
-  times.appcache = `DNS 缓存时间：${t.domainLookupStart - t.fetchStart}`;
-
-  // 卸载页面的时间
-  times.unloadEvent = `卸载页面的时间：${
-    t.unloadEventEnd - t.unloadEventStart
-  }`;
-
-  // TCP 建立连接完成握手的时间
-  times.connect = `TCP 建立连接完成握手的时间：${
-    t.connectEnd - t.connectStart
-  }`;
-
-  console.log(times);
-  return times;
-}
-// getPerformanceTiming();
 
 // 控制台输出
 document.addEventListener("keydown", e => {
@@ -128,30 +28,83 @@ document.addEventListener("keydown", e => {
     );
   }
 });
+
+onMounted(() => {
+  let viewport = document.getElementById("viewport");
+  let screenChange = document.getElementById("screen-change");
+  let dw = 0;
+  if (document.body.clientHeight > document.body.clientWidth) {
+    dw = 750; //页面的高度
+    screenChange!.classList.remove("h");
+    screenChange!.classList.add("w");
+  } else if (document.body.clientHeight < document.body.clientWidth) {
+    dw = 1334; //页面的宽度
+  }
+  viewport!.setAttribute("content", "width=" + dw + ", user-scalable=no");
+  window.addEventListener(
+    "orientationchange",
+    function () {
+      if (window.orientation == 0) {
+        screenChange!.classList.remove("h");
+        screenChange!.classList.add("w");
+        dw = 750; //页面的高度
+        viewport!.setAttribute("content", "width=" + dw + ", user-scalable=no");
+      } else if (window.orientation == 90) {
+        screenChange!.classList.remove("w");
+        screenChange!.classList.add("h");
+        dw = 1334; //页面的宽度
+        viewport!.setAttribute("content", "width=" + dw + ", user-scalable=no");
+      }
+    },
+    false
+  );
+});
 </script>
 
 <template>
-  <the-circular-nav />
-  <the-back-top />
-  <the-right-menu />
+  <div id="screen-change" class="h">
+    <the-circular-nav />
+    <the-back-top />
+    <the-right-menu />
 
-  <router-view v-slot="{ Component, route }">
-    <transition name="scale" mode="out-in">
-      <keep-alive>
-        <component :is="Component" :key="route.path" />
-      </keep-alive>
-    </transition>
-    <transition name="scale" mode="out-in">
-      <component
-        :is="Component"
-        :key="route.path"
-        v-if="!route.meta.keepAlive"
-      />
-    </transition>
-  </router-view>
+    <router-view v-slot="{ Component, route }">
+      <transition name="scale" mode="out-in">
+        <keep-alive>
+          <component :is="Component" :key="route.path" />
+        </keep-alive>
+      </transition>
+      <transition name="scale" mode="out-in">
+        <component
+          :is="Component"
+          :key="route.path"
+          v-if="!route.meta.keepAlive"
+        />
+      </transition>
+    </router-view>
+  </div>
 </template>
 <!-- todo 移动端适配，尝试改为横屏显示 -->
 <style>
+#screen-change {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  overflow: hidden;
+  z-index: 10;
+}
+#screen-change.w {
+  height: 100vw;
+  width: 100vh;
+  transform: translate(-50%, -50%) rotate(90deg);
+  -webkit-transform: translate(-50%, -50%) rotate(90deg);
+}
+#screen-change.h {
+  height: 100vh;
+  width: 100vw;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+}
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s ease;
