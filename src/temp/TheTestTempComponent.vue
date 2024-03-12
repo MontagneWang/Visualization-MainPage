bde5d34cc8338a0ef1ef08490b9d6ce183019f27
山遥路远 <2214981880@qq.com>
2024-03-12 17:12:00 +0800

    feat: 优化流式传输

--- src/temp/TheTestTempComponent.vue
+++ src/temp/TheTestTempComponent.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import {useCounterStore} from '../stores'
-import {watchEffect} from "vue";
+import {ref,watchEffect} from "vue";
 
 defineProps<{ msg: string }>()
 
@@ -8,6 +8,65 @@ let Counter
 watchEffect(() => {
 	Counter = useCounterStore()
 })
+
+// 分块请求, 部署到服务器时无法获取到 contentLength
+let handleRequest3 = async (size: number) => {
+  let time = performance.now();
+
+  // 每个请求块的大小，单位是字节。
+  const CHUNK_SIZE = size * 1024; // size KB
+
+  const dataUrl = "/data0623large.json";
+  const chunks = ref<string[]>([]);
+
+  // 先获取文件总大小
+  const fetchTotalSize = async () => {
+    const response = await fetch(dataUrl, { method: "HEAD" });
+    const contentLength = response.headers.get("Content-Length");
+
+    if (contentLength === null) {
+      throw new Error("Content-Length header is missing");
+    }
+
+    return Number(contentLength);
+  };
+
+  const fetchChunk = async (start: number, end: number) => {
+    const response = await fetch(dataUrl, {
+      headers: {
+        Range: `bytes=${start}-${end}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const text = await response.text();
+    chunks.value.push(text);
+  };
+
+  const totalSize = await fetchTotalSize();
+  const chunkCount = Math.ceil(totalSize / CHUNK_SIZE);
+
+  for (let i = 0; i < chunkCount; i++) {
+    const start = i * CHUNK_SIZE;
+    const end = Math.min((i + 1) * CHUNK_SIZE - 1, totalSize - 1);
+
+    try {
+      await fetchChunk(start, end);
+    } catch (error) {
+      console.error(`Failed to fetch chunk ${i + 1}:`, error);
+    }
+  }
+
+  const fullText = chunks.value.join("");
+  const fullJson = JSON.parse(fullText);
+
+  // Object.assign(data, generateData(columns, fullJson));
+  // loading.value = false;
+  console.log("分块请求:" + (performance.now() - time));
+};
 </script>
 
 <template>
--- src/views/IntroVocaloid.vue
+++ src/views/IntroVocaloid.vue
@@ -3,6 +3,29 @@ import { onMounted, reactive, ref } from "vue";
 import { TableV2FixedDir, TableV2SortOrder } from "element-plus";
 import type { SortBy } from "element-plus";
 
+// import { fetchStreamJson, arrayItemSymbol } from "stream-json-parse";
+
+// fetchStreamJson({
+//   // 请求地址
+//   url: "/data0623large.json",
+//   // 解析配置
+//   JSONParseOption: {
+//     // 要求完整解析对应路径下的数据，才能上报（可选）, arrayItemSymbol 表示数组项
+//     completeItemPath: ["data", arrayItemSymbol],
+//     // json解析的回调
+//     jsonCallback: (error, isDone, value) => {
+//       console.log("jsonCallback", error, isDone, value);
+//     },
+//     diffCallBack: (json, isEq) => {
+//       console.log("diffCallBack", json, isEq);
+//     },
+//   },
+//   // fetch请求配置，同浏览器 fetch api
+//   fetchOptions: {
+//     method: "GET",
+//   },
+// });
+
 interface Song {
   title: string;
   author: string;
@@ -20,6 +43,7 @@ interface Song {
   rank: number;
 }
 const loading = ref(true);
+let data = reactive<Song[]>([]);
 let columnsName = [
   { key: "title", name: "标题", width: 250 },
   { key: "author", name: "作者", width: 150 },
@@ -72,12 +96,28 @@ const generateData = (
 // 锁定表头表尾
 columns[0].fixed = TableV2FixedDir.LEFT;
 columns[13].fixed = TableV2FixedDir.RIGHT;
-let data = reactive<Song[]>([]);
+
 onMounted(async () => {
-  // todo 分割大请求，先请求小文件渲染一部分，再不断请求数据并更新
+	const response = await fetch("/data0623.json");
+  const reader = response.body!.getReader();
+  const stream = new ReadableStream({
+    async start(controller) {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          controller.close();
+          break;
+        }
+        controller.enqueue(value);
+      }
+    },
+  });
+  const result = await new Response(stream).json();
+  Object.assign(data, generateData(columns, result));
+  loading.value = false;
 });
 
-// 直接请求，需要 7s，页面卡死
+// 直接请求 页面会卡死
 let handleRequest1 = async () => {
   let time = performance.now();
   // const response = await fetch("/data0623.json"); // 1mb
@@ -87,13 +127,9 @@ let handleRequest1 = async () => {
   Object.assign(data, generateData(columns, fetchData));
   loading.value = false;
   console.log("直接请求:" + (performance.now() - time));
-  // todo 每个 添加计时模块
 };
 
 // fetch 流式传输 从7秒减少到3秒
-// todo 2 并发读取：你可以尝试使用多个读取器并发地从服务器读取数据。这可能需要服务器支持Range请求，这样每个读取器可以读取文件的不同部分。然而，这可能会使代码复杂化，因为你需要处理并发读取和数据合并。
-// todo 3 处理数据的同时读取数据：在你的代码中，你首先读取所有的数据，然后处理数据。你可以尝试边读取数据边处理数据，这样可能会更快。例如，如果你的数据是JSON格式的，你可以使用流式JSON解析器，如JSONStream或stream-json。
-// let data = reactive<Song[]>([]);
 let handleRequest2 = async () => {
   let time = performance.now();
   const response = await fetch("/data0623large.json");
@@ -116,63 +152,9 @@ let handleRequest2 = async () => {
   console.log("流式传输:" + (performance.now() - time));
 };
 
-// 分块请求
-let handleRequest3 = async (size: number) => {
-  let time = performance.now();
-
-  // 每个请求块的大小，单位是字节。
-  const CHUNK_SIZE = size * 1024; // size KB
-
-  const dataUrl = "/data0623large.json";
-  const chunks = ref<string[]>([]);
-
-  // 先获取文件总大小
-  const fetchTotalSize = async () => {
-    const response = await fetch(dataUrl, { method: "HEAD" });
-    const contentLength = response.headers.get("Content-Length");
-
-    // if (contentLength === null) {
-    //   throw new Error("Content-Length header is missing");
-    // }
-
-    return Number(contentLength);
-  };
-
-  const fetchChunk = async (start: number, end: number) => {
-    const response = await fetch(dataUrl, {
-      headers: {
-        Range: `bytes=${start}-${end}`,
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const text = await response.text();
-    chunks.value.push(text);
-  };
-
-  const totalSize = await fetchTotalSize();
-  const chunkCount = Math.ceil(totalSize / CHUNK_SIZE);
-
-  for (let i = 0; i < chunkCount; i++) {
-    const start = i * CHUNK_SIZE;
-    const end = Math.min((i + 1) * CHUNK_SIZE - 1, totalSize - 1);
-
-    try {
-      await fetchChunk(start, end);
-    } catch (error) {
-      console.error(`Failed to fetch chunk ${i + 1}:`, error);
-    }
-  }
-
-  const fullText = chunks.value.join("");
-  const fullJson = JSON.parse(fullText);
-
-  Object.assign(data, generateData(columns, fullJson));
-  loading.value = false;
-  console.log("分块请求:" + (performance.now() - time));
+// todo 流式传输时同步处理部分渲染，对返回的部分数据需要先修正为 JSON 格式
+let handleRequest3 = async () => {
+  
 };
 
 // 排序优化 [2].[4].[5].[6].[7].[8].[9].[10][12][13]
@@ -192,11 +174,9 @@ const onSort = (sortBy: SortBy) => {
     <div class="head">
       本次共收录 2023 上半年共 1084 首绫曲，您可以通过 [Shift + 鼠标滚轮]
       来左右滑动查看
-      <button @click="handleRequest1()">直接请求</button>
+      <!-- <button @click="handleRequest1()">直接请求</button>
       <button @click="handleRequest2()">流式传输</button>
-      <button @click="handleRequest3(500)">分块请求500k</button>
-      <button @click="handleRequest3(50)">分块请求50k</button>
-      <button @click="handleRequest3(1000)">分块请求1m</button>
+      <button @click="handleRequest3()">部分渲染</button> -->
     </div>
     <div style="height: 80vh; width: 90vw; margin: 9vh auto">
       <el-auto-resizer>
